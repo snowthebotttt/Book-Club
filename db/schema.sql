1fef1d175cbdfc4ce4752bdd4e06172ed26d6cbe
kpetrosky <kpetrosky36@gmail.com>
2023-04-27 20:43:28 -0700

    working trying to connect seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,11 @@
 DROP DATABASE IF EXISTS bookClub_db;
-
 CREATE DATABASE bookClub_db;
-
 USE bookClub_db;
-
 
 CREATE TABLE books (
   id INT NOT NULL,
   book_name VARCHAR(30) NOT NULL,
   author_name VARCHAR(30) NOT NULL,
-  rating_score INT NOT NULL
+  rating_score INT NOT NULL,
+  PRIMARY KEY (id)
 );
