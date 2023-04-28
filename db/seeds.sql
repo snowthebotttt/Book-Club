b877e75c748755a3a564f7d03410a8376a5e60b5
kpetrosky <kpetrosky36@gmail.com>
2023-04-27 20:00:17 -0700

    added db folder, added schema.sql. seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO products (id, book_name, author_name, rating_score)
+VALUES (001, "Harry Potter", "JK Rowling", 4),
+       (002, "Pout Pout Fish", "Deborah Diesen", 4),
+       (003, "HUnger Games", "Suzanne Collins", 3),
+       (004, "Carrie", "Stephen King", 3),
+       (005, "The Lair of the White Worm", "Bram Stoker", 1);
+       
