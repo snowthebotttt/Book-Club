35495e38c2c0a741281767e12bb7c5305cdb00b7
kpetrosky <kpetrosky36@gmail.com>
2023-04-27 20:25:29 -0700

    added data to schema, seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,13 @@
 DROP DATABASE IF EXISTS bookClub_db;
 
 CREATE DATABASE bookClub_db;
+
+USE bookClub_db;
+
+
+CREATE TABLE books (
+  id INT NOT NULL,
+  book_name VARCHAR(30) NOT NULL,
+  author_name VARCHAR(30) NOT NULL,
+  rating_score INT NOT NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,9 @@
-INSERT INTO products (id, book_name, author_name, rating_score)
-VALUES (001, "Harry Potter", "JK Rowling", 4),
-       (002, "Pout Pout Fish", "Deborah Diesen", 4),
-       (003, "HUnger Games", "Suzanne Collins", 3),
-       (004, "Carrie", "Stephen King", 3),
-       (005, "The Lair of the White Worm", "Bram Stoker", 1);
+INSERT INTO bookClub_db (id, book_name, author_name, rating_score)
+VALUES (001, 'Harry Potter', 'JK Rowling', 4),
+       (002, 'Pout Pout Fish', 'Deborah Diesen', 4),
+       (003, 'Hunger Games', 'Suzanne Collins', 3),
+       (004, 'Carrie', 'Stephen King', 3),
+       (005, 'The Lair of the White Worm', 'Bram Stoker', 1);
+
+       
        
