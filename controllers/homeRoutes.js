f98ef839c5accd82a9cf283a4d4537bd2efd1fb9
Jared Gergen <gergen.jared1@gmail.com>
2023-05-08 20:45:52 -0700

    W.e

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,27 @@
-
 const express = require("express");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
 
-router.get('/', function (req, res) {
-    res.render('homepage', {});
-  });
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      projects,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,14 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
 
-router.get('/', function (req, res) {
-    res.render('homepage', {});
-  });
+router.get("/", function (req, res) {
+  res.render("homepage", {});
+});
+
+router.get("/login", function (req, res) {
+  res.render("login", {});
+});
 
 module.exports = router;
