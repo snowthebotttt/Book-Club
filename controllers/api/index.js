81b8b0838a64a013f24eb3a9946abff67f957021
kpetrosky <kpetrosky36@gmail.com>
2023-05-08 19:45:10 -0700

    trying to make the app show up without errors

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Import middleware modules
-const login = require('./loginInformation');
+// const login = require('../../public/js/login')
 
 // Import route modules
 const userRoutes = require('./usersController');
